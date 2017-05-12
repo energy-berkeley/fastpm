0a535f812da9b326185f86c615ab4452b4f14161
Man-Yat Chu <energy@waterfall.dyn.berkeley.edu>
2017-05-12 10:43:03 -0700

    change runtest to run 1 test only.

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -8,13 +8,13 @@ FASTPM=`dirname $0`/../src/fastpm
 
 set -x
 
-mpirun -n 4 $FASTPM fastPM20.lua ic || fail
+#mpirun -n 4 $FASTPM fastPM20.lua ic || fail
 
-mpirun -n 4 $FASTPM fastPM20.lua fastpm lineark || fail
-mpirun -n 4 $FASTPM fastPM20.lua fastpm whitenoisek || fail
-mpirun -n 4 $FASTPM fastPM20.lua fastpm || fail
+#mpirun -n 4 $FASTPM fastPM20.lua fastpm lineark || fail
+#mpirun -n 4 $FASTPM fastPM20.lua fastpm whitenoisek || fail
+#mpirun -n 4 $FASTPM fastPM20.lua fastpm || fail
 
 
-mpirun -n 4 $FASTPM fastPM20.lua fastpm || fail
+mpirun -n 4 $FASTPM fastPM20.lua fastpm || fail 
 
 
